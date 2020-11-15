c908f1cc734b98e75c86ae7e7e49705aaba74973
marridG <1687450542@qq.com>
2020-11-15 12:11:51 +0800

    add combining multiple images into single view in multi_img_view.py

---
+++ Sudoku/assist/multi_img_view.py
@@ -0,0 +1,74 @@
+import typing
+import matplotlib
+from matplotlib import figure
+from matplotlib import pyplot as plt
+
+
+def multi_img_view(images: list, subtitles: list,
+                   row_cnt: int, col_cnt: int,
+                   title: str = "MULTI-IMG VIEW",
+                   fig_size: typing.Tuple[int, int] = None,
+                   close_all: bool = True) \
+        -> matplotlib.figure.Figure:
+    """
+    Combine and Show Several Images Together
+    :param images:          list of images
+    :param subtitles:       list of the subtitles of the images
+    :param row_cnt:         number of rows
+    :param col_cnt:         number of columns
+    :param title:           title of the combine image
+    :param fig_size:        size of the figure, (width, height)
+    :param close_all:       whether to close all figures during initialization
+    :return:                plt, fig
+    """
+    if close_all:
+        plt.close("all")
+    if len(images) != len(subtitles):
+        raise RuntimeError("[Error] Images Count and Subtitles Count Mismatch")
+    if len(images) > row_cnt * col_cnt:
+        raise RuntimeError("[Error] Images Count Overflow")
+
+    if fig_size is not None:
+        fig, _ax = plt.subplots(nrows=row_cnt, ncols=col_cnt, figsize=fig_size)
+    else:
+        fig, _ax = plt.subplots(nrows=row_cnt, ncols=col_cnt)
+    ax = _ax.flatten()
+
+    # Subplot Styles: remove spines, x/y-ticks
+    for _subplot_ax in ax:
+        _subplot_ax.spines['top'].set_visible(False)
+        _subplot_ax.spines['right'].set_visible(False)
+        _subplot_ax.spines['bottom'].set_visible(False)
+        _subplot_ax.spines['left'].set_visible(False)
+        _subplot_ax.set_xticks([])
+        _subplot_ax.set_yticks([])
+
+    # Show Images & Subtitles
+    for _img_idx, (_img, _img_title) in enumerate(zip(images, subtitles)):
+        ax[_img_idx].imshow(_img)
+        ax[_img_idx].set_title(_img_title)
+        ax[_img_idx].set_xticks([])
+        ax[_img_idx].set_yticks([])
+
+    fig.suptitle(title)
+    # fig.tight_layout()
+
+    return fig
+
+
+if "__main__" == __name__:
+    import cv2
+
+    img = cv2.imread("../imgs/sudoku_puzzle.jpg")
+
+    comb_view_fig = multi_img_view(
+        images=[img for _ in range(5)], subtitles=["test" for _ in range(5)],
+        row_cnt=2, col_cnt=3, title="123", fig_size=None, close_all=True)
+    comb_view_fig.tight_layout()
+    plt.show()
+
+    comb_view_fig = multi_img_view(
+        images=[img for _ in range(5)], subtitles=["test" for _ in range(5)],
+        row_cnt=2, col_cnt=3, title="123", fig_size=None)
+
+    plt.show()
