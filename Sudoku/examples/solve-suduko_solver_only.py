6d5dc5f02e831c3a58669dd10971847d62c51f20
marridG <1687450542@qq.com>
2020-11-28 22:20:07 +0800

    finish backtracking method & examples

--- Sudoku/examples/solve-suduko_solver_only.py
+++ Sudoku/examples/solve-suduko_solver_only.py
@@ -18,9 +18,33 @@ test_board_valid = [[8, -99, -99, -99, 1, -99, -99, -99, 9],
                     [-99, -99, 7, -99, 4, -99, 6, -99, -99],
                     [-99, 8, -99, 3, -99, 9, -99, 4, -99],
                     [3, -99, -99, -99, 5, -99, None, -99, 8]]  # valid board
+test_board_easy_1 = [[8, 7, 2, 4, 1, 3, 5, 6, 9],
+                     [9, 5, 6, 8, 2, 7, 3, 1, 4],
+                     [1, 3, 4, 6, 9, 5, 7, 8, 2],
+                     [4, 6, 9, 7, 3, 1, 8, 2, 5],
+                     [5, 2, 8, 9, 6, 4, 1, 3, 7],
+                     [7, 1, 3, 5, 8, 2, 4, 9, 6],
+                     [2, 9, 7, 1, 4, 8, 6, 5, 3],
+                     [6, 8, 5, 3, 7, 9, 2, 4, 1],
+                     [3, 4, 1, 2, 5, 6, 9, 7, 8]]  # valid full board
+test_board_easy_2 = [[8, 7, 2, 4, 1, 3, 5, 6, 9],
+                     [9, 5, 6, 8, 2, 7, 3, 1, 4],
+                     [1, 3, 4, 6, 9, 5, 7, 8, 2],
+                     [4, 6, 9, 7, 3, 1, 8, 2, 5],
+                     [5, 2, 8, 9, 6, 4, 1, 3, 7],
+                     [7, 1, 3, 5, 8, 2, 4, 9, 6],
+                     [2, 9, 7, 1, 4, 8, 6, 5, 3],
+                     [6, 8, 5, 3, 7, 9, 2, 4, None],
+                     [3, 4, 1, 2, 5, 6, 9, None, 8]]  # valid to-be-filled board
 test_board = np.array(test_board_valid)
+# test_board = np.array(test_board_easy_1)
+# test_board = np.array(test_board_easy_2)
 # test_board = np.array(test_board_invalid_1)
 # test_board = np.array(test_board_invalid_2)
 # test_board = np.array(test_board_invalid_3)
-sb = sudoku_board.SudokuBoard(board=test_board)
-sb_solver = sudoku_solver.SudokuSolver(sudoku_board=sb)
+sb = sudoku_board.SudokuBoard(board=test_board,
+                              invalid_tolerable=False, show_info=True)
+sb_solver = sudoku_solver.SudokuSolver()
+solved, solved_board = sb_solver.backtrack(board=sb, recheck=True)
+print(solved)
+print(solved_board.output_board_as_str(line_prefix="\t"))
--- Sudoku/solve_suduko/sudoku_solver.py
+++ Sudoku/solve_suduko/sudoku_solver.py
@@ -5,23 +5,74 @@ from .sudoku_board import SudokuBoard
 
 
 class SudokuSolver:
-    def __init__(self, sudoku_board: SudokuBoard):
-        self.board_obj = deepcopy(sudoku_board)
+    def __init__(self):
+        # Initiate with empty SudokuBoard, to get constants only
+        self.board = SudokuBoard(board=np.full((9, 9), -99, dtype=np.int),
+                                 invalid_tolerable=False, show_info=False)
+
+    def backtrack(self, board: SudokuBoard, recheck: bool = True):
+        board_flattened = board.board.flatten()  # <np.ndarray>, shape (cells,)
+
+        # Find 1ST empty cell (in the flattened board) to save time
+        # result shape (hit,); or using np.where()
+        empty_cells_flt_idx = np.argwhere(self.board.EMPTY_LABEL == board_flattened)
+        # [CASE I] all nonempty, i.e., full Sudoku
+        # (actually guaranteed to be valid since checked @SudokuBoard)
+        if 0 == empty_cells_flt_idx.size:
+            print("[INFO] Given a Full and Valid Sudoku Board")
+            return True, board
+        # [CASE II] not all nonempty
+        start_idx = np.min(empty_cells_flt_idx)
+
+        to_solve_board = deepcopy(board)
+        # test codes to show that deepcopy() works
         # self.board_obj.board[0, 0] = -100
         # print(sudoku_board.board[0, 0], self.board_obj.board[0, 0])
 
-    def backtrack(self):
-        pass
+        solved, solved_board = self.__backtrack__(
+            in_board=to_solve_board, to_fill_idx=start_idx, recheck=recheck)
+        return solved, solved_board
 
-    def __backtrack__(self, in_board: SudokuBoard, fill_target_idx: int):
+    def __backtrack__(self, in_board: SudokuBoard, to_fill_idx: int, recheck: bool = True):
         """
-        Recursively backtracking
+        Recursively backtracking (ALERT: carefully handle referred <SudokuBoard>)
+        Notice, the recursion indentifier is the FLATTENED index, as,
+            similar e.g. [[0, 1, 2], [3, 4, 5]] => [0, 1, 2, 3, 4, 5]
         :param in_board:            Input board Object
-        :param fill_target_idx:     Index of the to-fill cell,
+        :param to_fill_idx:         FLATTENED Index of the to-fill cell,
                                         ranging in {0, 1, ..., MAX*MAX-1}
+        :param recheck:             Whether to recheck the board in the base case
         :return:
         """
-        board = deepcopy(in_board)
+        # Map flattened index to (row, col)
+        to_fill_by_row = to_fill_idx // self.board.BOARD_SIZE
+        to_fill_by_col = to_fill_idx - to_fill_by_row * self.board.BOARD_SIZE
 
-        fill_target_row = fill_target_idx // self.board_obj.BOX_SIZE
-        fill_target_col = fill_target_idx - fill_target_row * self.board_obj.BOX_SIZE
+        # Recursion base case
+        if to_fill_idx >= self.board.board_cells_cnt:
+            solved = True
+            if recheck:
+                solved, _ = in_board.check_board_is_valid()
+            if solved:
+                return True, in_board
+            else:
+                return False, None
+
+        # Continue with non-empty cells
+        if self.board.EMPTY_LABEL != in_board.board[to_fill_by_row, to_fill_by_col]:
+            return self.__backtrack__(
+                in_board=in_board, to_fill_idx=to_fill_idx + 1, recheck=recheck)
+
+        # Find & try each of the possible fill-in numbers of empty cells
+        possible_nums = in_board.find_cell_possible_nums(
+            row_idx=to_fill_by_row, col_idx=to_fill_by_col)
+        for _num in possible_nums:
+            filled_board = deepcopy(in_board)  # DEEP COPY REQUIRED
+            filled_board.board[to_fill_by_row, to_fill_by_col] = _num
+            solved, solved_board = self.__backtrack__(
+                in_board=filled_board, to_fill_idx=to_fill_idx + 1, recheck=recheck)
+            if solved:
+                return True, solved_board
+
+        # All cases failed, solution NOT found
+        return False, None
