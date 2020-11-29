f103ef083df17d141b17f192a89dfce6c9ce5ce2
marridG <1687450542@qq.com>
2020-11-29 23:22:21 +0800

    update board extraction examples

--- examples/extract_board.py
+++ examples/extract_board.py
@@ -2,8 +2,7 @@ import cv2
 from extract_sudoku import board_img_arr
 
 # using wrapped API of opencv-sudoku-solver
-
-test_image = cv2.imread("1-2__from_lower.jpg")
+test_image = cv2.imread("sudoku_puzzle.jpg")
 bia = board_img_arr.BoardImgArr(img=test_image,
                                 debug_board_detection=False,
                                 debug_digits_extraction=False,
