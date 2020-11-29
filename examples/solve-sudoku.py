47b33d1ef6270e00b657faf7b9a2cb896ade297e
marridG <1687450542@qq.com>
2020-11-29 23:27:26 +0800

    update solve-sudoku examples; rename solve_suduko to solve_sudoku

--- examples/solve-sudoku.py
+++ examples/solve-sudoku.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 # import modules
-from solve_suduko import sudoku_board
+from solve_sudoku import sudoku_board, sudoku_solver
 
 test_board_valid = [[8, -99, -99, -99, 1, -99, -99, -99, 9],
                     [-99, 5, -99, 8, -99, 7, -99, 1, -99],
@@ -40,13 +40,36 @@ test_board_invalid_3 = [[8, -99, -99, -99, 1, -99, -99, -99, 9],
                         [-99, -99, 7, -99, 4, -99, 6, -99, -99],
                         [-99, 2, -99, 3, -99, 9, -99, 8, -99],
                         [3, -99, -99, -99, 5, -99, None, None, 8]]  # invalid board at box (2, 2)
+test_board_easy_1 = [[8, 7, 2, 4, 1, 3, 5, 6, 9],
+                     [9, 5, 6, 8, 2, 7, 3, 1, 4],
+                     [1, 3, 4, 6, 9, 5, 7, 8, 2],
+                     [4, 6, 9, 7, 3, 1, 8, 2, 5],
+                     [5, 2, 8, 9, 6, 4, 1, 3, 7],
+                     [7, 1, 3, 5, 8, 2, 4, 9, 6],
+                     [2, 9, 7, 1, 4, 8, 6, 5, 3],
+                     [6, 8, 5, 3, 7, 9, 2, 4, 1],
+                     [3, 4, 1, 2, 5, 6, 9, 7, 8]]  # valid full board
+test_board_easy_2 = [[8, 7, 2, 4, 1, 3, 5, 6, 9],
+                     [9, 5, 6, 8, 2, 7, 3, 1, 4],
+                     [1, 3, 4, 6, 9, 5, 7, 8, 2],
+                     [4, 6, 9, 7, 3, 1, 8, 2, 5],
+                     [5, 2, 8, 9, 6, 4, 1, 3, 7],
+                     [7, 1, 3, 5, 8, 2, 4, 9, 6],
+                     [2, 9, 7, 1, 4, 8, 6, 5, 3],
+                     [6, 8, 5, 3, 7, 9, 2, 4, None],
+                     [3, 4, 1, 2, 5, 6, 9, None, 8]]  # valid to-be-filled board
+
 test_board = np.array(test_board_valid)
 # test_board = np.array(test_board_invalid_1)  # Failed at Column #8
 # test_board = np.array(test_board_invalid_2)  # Failed at Row #8
 # test_board = np.array(test_board_invalid_3)  # Failed at Box (row, col) = (2, 2)
+# test_board = np.array(test_board_easy_1)
+# test_board = np.array(test_board_easy_2)
+
+# === BOARD ===
 sb = sudoku_board.SudokuBoard(board=test_board)
 # Stored Board
-print("Stored Board")
+print("Stored Board:")
 print(sb.board)
 # Possible Numbers of an Empty Cell
 print("Possible Numbers of Empty Cell (%d, %d): " % (8, 1), end="")
@@ -54,3 +77,11 @@ print(sb.find_cell_possible_nums(row_idx=8, col_idx=1))  # [2, 4, 9]
 # Output the Formatted Board
 print("Formatted Board (Indent by \"\\t\")")
 print(sb.output_board_as_str(line_prefix="\t"))
+print()
+
+# === SOLVER ===
+sb_solver = sudoku_solver.SudokuSolver()
+solved, solved_board = sb_solver.backtrack(board=sb, recheck=True)
+print("Solved =", solved)
+print("Solved Board:")
+print(solved_board.output_board_as_str(line_prefix="\t"))
---
+++ test.py
@@ -0,0 +1,3 @@
+from extract_sudoku import opencv__train
+
+opencv__train.train()
