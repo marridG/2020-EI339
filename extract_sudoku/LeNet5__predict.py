44e3f8d3fcf07396ff5284ca4803b6f05420a531
marridG <1687450542@qq.com>
2020-12-16 23:14:58 +0800

    add image transform in wrapper LeNet5__predict.py

--- extract_sudoku/LeNet5__predict.py
+++ extract_sudoku/LeNet5__predict.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import torch
+import torchvision
 
 from digit_classifiers import networks_structures, networks_models
 
@@ -15,19 +16,24 @@ class Predict:
         network = networks_structures.LeNet5(num_classes=num_classes).double()
         self.model = networks_models.NetworkModel(
             network=network, pre_trained_path=model_path)
+        self.transform = torchvision.transforms.Compose([
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize((0.1307,), (0.3081,))
+        ])
         print("[INFO] Digits Classifier Model Loaded")
 
     def predict(self, roi: np.ndarray):
         """
         Predict the number in the given region
-        :param roi:         Region-of-Interest, should be of shape (1,1,28,28)
+        :param roi:         Region-of-Interest, should be of shape (28,28)
         :return:            Prediction
         """
-        img = torch.from_numpy(roi).double()
+        img = self.transform(roi)
+        img = img.reshape(1, 1, 28, 28).double()
         return self.model.predict(img=img)
 
 
 if "__main__" == __name__:
     pred_obj = Predict(model_path="../models/LeNet5__lr=0.001__epoch=1.pth")
-    pred = pred_obj.predict(roi=np.random.random((1, 1, 28, 28)))
+    pred = pred_obj.predict(roi=np.random.random((28, 28)))
     print(pred)
