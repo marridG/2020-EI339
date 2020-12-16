750cd8ca8328cb40e945697020d91d3f715533c0
marridG <1687450542@qq.com>
2020-12-16 19:42:25 +0800

    update LeNet5 train & test codes

---
+++ extract_sudoku/LeNet5__train.py
@@ -0,0 +1,98 @@
+import torch
+from torch.utils.data.dataloader import DataLoader
+import torchvision
+import numpy as np
+
+from data import load_local_dataset
+from digit_classifiers import networks_structures, networks_models
+
+output_path = "./models"
+
+
+def train(model_path: str = output_path, dataset_settings: list = None,
+          batch_size: int = 30, lr: float = 1e-3, epoch: int = 1):
+    print("Hyper-parameters: batch_size=%d, lr=%.5f, epoch=%d" % (batch_size, lr, epoch))
+
+    transform = torchvision.transforms.Compose([
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize((0.1307,), (0.3081,))
+    ])
+    # train_mnist_loader = DataLoader(
+    #     torchvision.datasets.MNIST('../data', train=True, download=True, transform=transform),
+    #     batch_size=batch_size, shuffle=True)
+    train_mnist_loader = DataLoader(
+        dataset=load_local_dataset.TorchLocalDataLoader(
+            train=True, transform=transform, mnist=True, ei339=False),
+        batch_size=batch_size, shuffle=True)
+    train_ei339_loader = DataLoader(
+        dataset=load_local_dataset.TorchLocalDataLoader(
+            train=True, transform=transform, mnist=False, ei339=True),
+        batch_size=batch_size, shuffle=True)
+    train_all_loader = DataLoader(
+        dataset=load_local_dataset.TorchLocalDataLoader(
+            train=True, transform=transform, mnist=True, ei339=True),
+        batch_size=batch_size, shuffle=True)
+    test_mnist_loader = DataLoader(
+        dataset=load_local_dataset.TorchLocalDataLoader(
+            train=False, transform=transform, mnist=True, ei339=False),
+        batch_size=batch_size, shuffle=True)
+    test_ei339_loader = DataLoader(
+        dataset=load_local_dataset.TorchLocalDataLoader(
+            train=False, transform=transform, mnist=False, ei339=True),
+        batch_size=batch_size, shuffle=True)
+    test_all_loader = DataLoader(
+        dataset=load_local_dataset.TorchLocalDataLoader(
+            train=False, transform=transform, mnist=True, ei339=True),
+        batch_size=batch_size, shuffle=True)
+
+    dataset_labels = ["MNIST", "EI339", "MNIST+EI339"]
+    dataset_label_to_loader = {"train": {"MNIST": train_mnist_loader,
+                                         "EI339": train_ei339_loader,
+                                         "MNIST+EI339": train_all_loader},
+                               "test": {"MNIST": test_mnist_loader,
+                                        "EI339": test_ei339_loader,
+                                        "MNIST+EI339": test_all_loader}, }
+    report = [
+        # {"train_set": "MNIST", "test_set": "MNIST",
+        #  "batch_size": 30, "lr": 1e-3, "epoch": 1,
+        #  "train_loss": np.array([0.3, 0.2, 0.1]), "train_acc": np.array([70.4, 80.1, 90.5]),
+        #  "test_acc": 80.1, },
+    ]
+
+    for (train_name, test_name) in dataset_settings:
+        assert train_name in dataset_labels, \
+            "[Error] Invalid Train Name \"%s\". Supported are %s" \
+            % (train_name, ", ".join(dataset_labels))
+        assert test_name in dataset_labels, \
+            "[Error] Invalid Test Name \"%s\". Supported are %s" \
+            % (test_name, ", ".join(dataset_labels))
+
+        print("Train upon %s, Test upon %s" % (train_name, test_name))
+        train_loader = dataset_label_to_loader["train"][train_name]
+        test_loader = dataset_label_to_loader["test"][test_name]
+        network = networks_structures.LeNet5(num_classes=20)
+        model = networks_models.NetworkModel(network=network,
+                                             optimizer=torch.optim.SGD(
+                                                 params=network.parameters(), lr=lr, momentum=0.9))
+        train_loss, train_acc = model.train(train_loader=train_loader, num_epoch=epoch)
+        output_model_fn = model.output_model(path=model_path,
+                                             filename_prefix="LeNet5__lr=%.5f__epoch=%d__batch_size=%d" %
+                                                             (lr, epoch, batch_size),
+                                             min_loss=np.min(train_loss),
+                                             loss_each_epoch=train_loss,
+                                             accuracy_each_epoch=train_acc,
+                                             comments="", other_fields=None)
+        print("\tModel Trained Upon %s, Saved at \"%s\"" % (train_name, output_model_fn))
+        test_acc = model.evaluate(data_loader=test_loader)
+        print("\tTest Accuracy Upon %s: %.2f%%" % (test_name, test_acc))
+
+        report.append({"train_set": train_name, "test_set": test_name,
+                       "model_fn": output_model_fn,
+                       "batch_size": batch_size, "lr": lr, "epoch": epoch,
+                       "train_loss": train_loss, "train_acc": train_acc,
+                       "test_acc": test_acc, })
+    return report
+
+
+if "__main__" == __name__:
+    print(train())
--- test.py
+++ test.py
@@ -1,30 +1,15 @@
 import torch
-from torch.utils.data.sampler import SubsetRandomSampler
 from torch.utils.data.dataloader import DataLoader
-from torchvision import datasets, transforms
 import torchvision
 import numpy as np
-from extract_sudoku import opencv__train
-from digit_classifiers import networks_structures, network_model
+from extract_sudoku import opencv__train, LeNet5__train
+from digit_classifiers import networks_structures, networks_models
+from data import load_local_dataset
 
 # opencv__train.train()
 
-
-train_loader = DataLoader(
-    datasets.MNIST('./data', train=True, download=True,
-                   transform=transforms.Compose([
-                       transforms.ToTensor(),
-                       transforms.Normalize((0.1307,), (0.3081,))
-                   ])),
-    batch_size=30, shuffle=True)
-
-network = networks_structures.LeNet5(num_classes=20)
-network_model = network_model.NetworkModel(network=network)
-train_loss, train_acc = network_model.train(train_loader=train_loader, num_epoch=1)
-output_model = network_model.output_model(path="./models/",
-                                          filename_prefix="LeNet5__lr=0.001",
-                                          min_loss=np.min(train_loss),
-                                          loss_each_epoch=train_loss,
-                                          accuracy_each_epoch=train_acc,
-                                          comments="", other_fields=None)
-print(output_model)
+dataset_labels = ["MNIST", "EI339", "MNIST+EI339"]
+# dataset_settings = [(i, j) for i in dataset_labels for j in dataset_labels]
+dataset_settings = [("MNIST+EI339", "MNIST+EI339")]
+LeNet5__train.train(model_path="./models/", dataset_settings=dataset_settings,
+                    batch_size=30, lr=1e-3, epoch=1)
